a4cbdf76f6bdc918ba5206093751537eb23314a7
joeczar <joe.czar@gmail.com>
2019-01-25 14:18:10 +0100

    add ...

---
+++ jsx/src/index.js
@@ -0,0 +1,15 @@
+    //import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+    // Create a react component
+const App = () => {
+    return <div>Hello World!</div>;
+};
+
+    // Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+
+);
