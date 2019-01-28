017ff74af91ae9418ff8ddfff24a88dbf84e0de5
Joe Czarnecki <joe.czar@gmail.com>
2019-01-28 18:27:40 +0100

    definded constructor method, initialized state object

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = { lat: null };
+    }
+    //react says we have to define render!!
+   render() {
     window.navigator.geolocation.getCurrentPosition(
         (position) => console.log(position),
         (err) => console.log(err)
     );
-    return <div><h1>Howdy pardner!</h1></div>
-
-};
+      return <div>Latitude: </div>;
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
