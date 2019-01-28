3130c379b558a65a06896e31d88b97c5ecdd4ca6
Joe Czarnecki <joe.czar@gmail.com>
2019-01-28 16:25:48 +0100

    added ApprovalCard.js in components

--- components/src/index.js
+++ components/src/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard'
 
 // Create a react component
 const App = () => {
  
   return (
     <div className="ui container comments">
+        <ApprovalCard />
         <CommentDetail 
             author="Sam" 
             timeAgo="Today at 4:45PM" 
