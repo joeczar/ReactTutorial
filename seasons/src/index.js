e8f36c4ff78455c6a9f4b19c0a45446ff927260c
Joe Czarnecki <joe.czar@gmail.com>
2019-01-28 17:54:21 +0100

    added Geolocation API

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
     return <div><h1>Howdy pardner!</h1></div>
 
 };
