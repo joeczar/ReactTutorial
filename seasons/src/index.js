fbc2172b4ea7a3a5e7f220d4c04625bee7f7af18
Joe Czarnecki <joe.czar@gmail.com>
2019-01-29 10:33:15 +0100

    moved geolocatation call from constructor to componentDidMount

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,17 +7,15 @@ class App extends React.Component {
 
         //THIS IS THE ONLY TIME we do direct assignment to this state!!!!
         this.state = { lat: null, errorMessage: '' };
+    }
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // we called setState!!!
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
+
     //react says we have to define render!!
    render() {
      if (this.state.errorMessage && !this.state.lat) {
