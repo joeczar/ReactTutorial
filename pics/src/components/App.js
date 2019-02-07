300df22f3bb1340e89dc3b2619067a894d41237d
Joe Czarnecki <joe.czar@gmail.com>
2019-02-07 11:58:38 +0100

    added unsplash api to App

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,8 +3,12 @@ import Axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-onSearchSubmit(term) {
-    console.log(term);
+    onSeaaxios.get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+            Authorization: 'Client-ID 0bebbca34b7bc6e376686b18833cd7a02a3b86a67a2d3381b7210a43c7e6952a' 
+        }
+    });
 }
 
     render() { 
