ed152efafc8781aafecff7091090ea4362809990
Joe Czarnecki <joe.czar@gmail.com>
2019-02-08 13:26:22 +0100

    removed .then() added async await and response const

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,16 +3,15 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 
                     'Client-ID 0bebbca34b7bc6e376686b18833cd7a02a3b86a67a2d3381b7210a43c7e6952a' 
             }
-        }).then((response) => {
-            console.log(response.data.results);
         });
+        console.log(response.data.results);
     }
 
     render() { 
