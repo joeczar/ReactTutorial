7bcc3c5495322de6dc81004bcb8fc92882283d77
Joe Czarnecki <joe.czar@gmail.com>
2019-02-08 13:21:51 +0100

    added .then() to axios call

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSeaaxios.get('https://api.unsplash.com/search/photos', {
-        params: { query: term },
-        headers: {
-            Authorization: 'Client-ID 0bebbca34b7bc6e376686b18833cd7a02a3b86a67a2d3381b7210a43c7e6952a' 
-        }
-    });
-}
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 
+                    'Client-ID 0bebbca34b7bc6e376686b18833cd7a02a3b86a67a2d3381b7210a43c7e6952a' 
+            }
+        }).then((response) => {
+            console.log(response.data.results);
+        });
+    }
 
     render() { 
         return (
