fd9f1de6c1315f0a91325d627614ee7d4f962d7b
Joe Czarnecki <joe.czar@gmail.com>
2019-02-16 08:46:03 +0100

    pulled SongList into App

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -5,7 +5,7 @@ const songsReducer = () => {
     return [
         { title:'No Scrubs', duration: '4:05' },
         { title:'Macerena', duration: '2:30' },
-        { title:'I Want it t', duration: '3:15' },
+        { title:'Chocolate Jesus', duration: '3:15' },
         { title:'I Want it That Way', duration: '1:45' },
     ];
 };
