9586c5f0133dff4cea33da6345e73fec158ec396
Joe Czarnecki <joe.czar@gmail.com>
2019-02-16 13:36:59 +0100

    added PostList

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PostList from './PostList'
 
 const App = () => {
     return (
         <div className="ui container">
-            <h1>App</h1>
+            <PostList />
         </div>
     );
 };
---
+++ blog/src/components/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>PostList</h2>
+            </div>
+        );
+    }
+}
+
+export default PostList;
